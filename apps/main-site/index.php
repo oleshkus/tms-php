51435fc25eee5aaffd51e4b85938a333b271b624
oleshkus <oleshkus@gmail.com>
2024-01-25 17:18:45 +0300

    начало

---
+++ apps/main-site/index.php
@@ -0,0 +1,26 @@
+<?php
+$x = 5;
+echo "<br>";
+echo $x+3;
+echo "<br>";
+
+$a = 10;
+$b = 7;
+$y = 12;
+$name = "Алиса";
+$age = 25;
+echo $a/$b . "<br>";
+$stroka = "Меня зовут $name, мне $age лет и ";
+echo $y*2 . "<br>";
+
+echo $stroka . "мне нравится программирование";
+
+$x = 6;
+$y = 4;
+$i = 0;
+echo $x*$y . "<br>";
+echo $x++ . "<br>";
+
+$num = 5;
+$notnum = "5";
+echo $num+$notnum;
